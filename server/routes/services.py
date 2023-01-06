f06e5f0cc77bcb09026628777aaf4ab6091e6c96
dewll <sodiqb86@gmail.com>
2023-01-06 11:11:15 +0100

    add status code to endpoint

--- server/routes/services.py
+++ server/routes/services.py
@@ -141,7 +141,7 @@ async def create_services(data:GoodsAndServiceEventSchema,response:Response,Auth
 
 
 @router.post("/delivery",status_code=201)
-async def create_delivery(data:DeliverySchema,Authorize: AuthJWT = Depends()) -> dict:
+async def create_delivery(data:DeliverySchema,response:Response, Authorize: AuthJWT = Depends()) -> dict:
     Authorize.jwt_required()
     current_user = Authorize.get_jwt_subject()
     
@@ -151,21 +151,24 @@ async def create_delivery(data:DeliverySchema,Authorize: AuthJWT = Depends()) ->
         image_obj = await upload_image_to_S3_bucket(data.images,DeliveryImages)
     else:
         image_obj = await upload_image_to_file_path(data.images,DeliveryImages)
-                
-    delivery = Delivery(
-        pick_up_location=data.pick_up_location,
-        delivery_location=data.delivery_location,
-        category=data.category,
-        delivery_type=data.delivery_type,
-        size=data.size,
-        select_category=data.select_category,
-        price=data.price,
-        description=data.description,
-        tags=data.tags,
-        image=image_obj,
-        owner_id=user.id
-        )
-    
-    await delivery.create()
-    
-    return {"message":"delivery successfully uploaded"}
+    try:            
+        delivery = Delivery(
+            pick_up_location=data.pick_up_location,
+            delivery_location=data.delivery_location,
+            category=data.category,
+            delivery_type=data.delivery_type,
+            size=data.size,
+            select_category=data.select_category,
+            price=data.price,
+            description=data.description,
+            tags=data.tags,
+            image=image_obj,
+            owner_id=user.id
+            )
+        
+        await delivery.create()
+        
+        return {"message":"delivery successfully uploaded"}
+    except:
+        response.status_code = 400
+        return {"message":"Something went wrong"}
