c82d5a997e970ef0d0405e49e5dfec26ea7bb569
dewll <sodiqb86@gmail.com>
2023-01-06 16:35:02 +0100

    make all explore into a single list

--- server/routes/explore.py
+++ server/routes/explore.py
@@ -3,6 +3,7 @@ from fastapi_jwt_auth import AuthJWT
 from server.models.services import (Product, Service, Event, Delivery)
 from beanie.operators import RegEx,And,Or
 from ..utils.location_manager import get_location
+import json
 router = APIRouter()
 
 @router.get("/all",status_code =200)
@@ -14,8 +15,17 @@ async def get_all_explore(Authorize: AuthJWT = Depends()) -> dict:
     service = await Service.find(fetch_links=True).to_list()
     event = await Event.find(fetch_links=True).to_list()
     delivery = await Delivery.find(fetch_links=True).to_list()
-        
-    return {"product":product,"service":service,"event":event,"delivery":delivery}
+    
+    all_service_list = []
+    all_service_list.extend(product)
+    all_service_list.extend(service)
+    all_service_list.extend(event)
+    all_service_list.extend(delivery)
+    
+    
+    return all_service_list
+    
+
 
 
 @router.get("/lat-long/{latitude}/{longitude}",status_code =200)
